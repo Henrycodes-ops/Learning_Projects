d1e33859aa3b110acb48735a62d8ceac021566b1
Falolu Henry <henryfalolu1@gmail.com>
2025-01-02 20:45:17 +0100

    New Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import React from "react";
 import "./App.css";
 import Header from "./assets/component/header";
 import Entry from "./assets/component/entry";
+import jokesData from './data'
 
 
 function App() {
+  const jokeElement = jokesData.map((joke) => {
+    return <joke />
+
+  })
   return (
     <div className="container">
       <Header />
       <Entry />
+      
     </div>
   );
 }
