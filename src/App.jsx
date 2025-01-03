03940a76eb21f69d1b8e9523b68f3538f0917cc4
Falolu Henry <henryfalolu1@gmail.com>
2025-01-03 19:21:11 +0100

    Added the joke.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import React from "react";
 import "./App.css";
 import Header from "./assets/component/header";
 import Entry from "./assets/component/entry";
-import jokesData from './data'
+import jokesData from './data.js'
+import Joke from "./joke.jsx";
 
 
 function App() {
   const jokeElement = jokesData.map((joke) => {
-    return <joke />
+    return <Joke setup={joke.setup} punchline={joke.punchline} />
 
   })
   return (
     <div className="container">
       <Header />
       <Entry />
-      
+      {jokeElement}
     </div>
   );
 }
